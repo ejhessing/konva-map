bad3f4b8c60bc9ff60c2aeab5889464080e97634
Erwin Hessing <ejhessing@gmail.com>
2022-11-01 23:12:36 +0700

    Try replace touch move for pinch

--- src/LocationMap/index.tsx
+++ src/LocationMap/index.tsx
@@ -134,71 +134,62 @@ export const LocationMap = ({
 
   function handleTouch(e: Konva.KonvaEventObject<TouchEvent>) {
     e.evt.preventDefault();
-
     var touch1 = e.evt.touches[0];
     var touch2 = e.evt.touches[1];
     const stage = stageRef.current;
-    if (stage !== null) {
-      if (touch1 && touch2) {
-        var p1 = {
-          x: touch1.clientX,
-          y: touch1.clientY,
-        };
-        var p2 = {
-          x: touch2.clientX,
-          y: touch2.clientY,
-        };
-        console.log({ p1, p2, lastCenter });
-        if (!lastCenter) {
-          lastCenter = getCenter(p1, p2);
-          // return;
-        }
-        var newCenter = getCenter(p1, p2);
-        console.log({ newCenter });
-        var dist = getDistance(p1, p2);
-        console.log({ dist, lastDist });
-        if (!lastDist) {
-          lastDist = dist;
-        }
-
-        // local coordinates of center point
-        var pointTo = {
-          x: (newCenter.x - stage.x()) / stage.scaleX(),
-          y: (newCenter.y - stage.y()) / stage.scaleX(),
-        };
-        console.log({ pointTo });
-        var scale = stage.scaleX() * (dist / lastDist);
-        console.log({ scale });
-        if (scale < 1) {
-          scale = 1;
-        }
-        stage.scaleX(scale);
-        stage.scaleY(scale);
-
-        // calculate new position of the stage
-        var dx = newCenter.x - lastCenter.x;
-        var dy = newCenter.y - lastCenter.y;
-        console.log({ dx, dy });
-        var newPos = {
-          x: newCenter.x - pointTo.x * scale + dx,
-          y: newCenter.y - pointTo.y * scale + dy,
-        };
-        console.log({ newPos });
-        // TODO: check if this is right on mobile
-        if (stage.scaleX() <= 1) {
-          newPos = {
-            x: lastCenter.x,
-            y: lastCenter.y,
-          };
-        }
-
-        stage.position(newPos);
-        stage.batchDraw();
-
-        lastDist = dist;
-        lastCenter = newCenter;
-        console.log({ lastDist, lastCenter });
+    if (!stage) return;
+    if (touch1 && touch2) {
+      // if the stage was under Konva's drag&drop
+      // we need to stop it, and implement our own pan logic with two pointers
+      if (stage.isDragging()) {
+        stage.stopDrag();
       }
+
+      var p1 = {
+        x: touch1.clientX,
+        y: touch1.clientY,
+      };
+      var p2 = {
+        x: touch2.clientX,
+        y: touch2.clientY,
+      };
+
+      if (!lastCenter) {
+        lastCenter = getCenter(p1, p2);
+        return;
+      }
+      var newCenter = getCenter(p1, p2);
+
+      var dist = getDistance(p1, p2);
+
+      if (!lastDist) {
+        lastDist = dist;
+      }
+
+      // local coordinates of center point
+      var pointTo = {
+        x: (newCenter.x - stage.x()) / stage.scaleX(),
+        y: (newCenter.y - stage.y()) / stage.scaleX(),
+      };
+
+      var scale = stage.scaleX() * (dist / lastDist);
+
+      stage.scaleX(scale);
+      stage.scaleY(scale);
+
+      // calculate new position of the stage
+      var dx = newCenter.x - lastCenter.x;
+      var dy = newCenter.y - lastCenter.y;
+
+      var newPos = {
+        x: newCenter.x - pointTo.x * scale + dx,
+        y: newCenter.y - pointTo.y * scale + dy,
+      };
+
+      stage.position(newPos);
+
+      lastDist = dist;
+      lastCenter = newCenter;
     }
   }
 
